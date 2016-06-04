9ebde419739d6657077126e67a6af5d34345a53a
Julien Batonnet <julien.batonnet@gmail.com>
2016-06-04 16:46:54 +0200

    Stream extension and object database

---
+++ Model/ObjectDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+
+public abstract class ObjectDatabase<T> : IEnumerable<T>
+{
+    public abstract ICollection<T> Objects { get; }
+
+    public abstract void Load();
+    public abstract void Save();
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return Objects?.GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
